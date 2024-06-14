694395d04fb7f9cbce01b0d6dbc93ac8176a367c
54nyawira <charitynyawirairungu@gmail.com>
2024-06-14 19:00:59 +0300

    validation is working

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [NavbarComponent]
 })
 export class UserComponent {
 
