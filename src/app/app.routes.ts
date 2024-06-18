80009290b3ca79c66f1de94022e4cc545bfb9a61
54nyawira <charitynyawirairungu@gmail.com>
2024-06-18 15:52:26 +0300

    added the CRUD operations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 export const routes: Routes = [
     {path:'',component:LandingComponent},
@@ -12,6 +16,9 @@ export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'register',component:RegisterComponent},
     {path:'user',component:UserComponent},
+    {path:'notes',component:NotesComponent},
     {path:'admin',component:AdminComponent},
-    {path:'**',component:NotFoundComponent}
+    
+    {path:'**',component:NotFoundComponent},
+   
 ];
